eb595bf21be1b61ef17082e3a23c3a6c975fc21e
JAY KUMAR <jaykumar@cdac.in>
2024-11-28 17:16:23 +0530

    Readme commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -39,7 +39,7 @@ const Login = ({ onLogin }) => {
                 }}
             >
                 <Typography variant="h4" mb={3} align="center">
-                    Welcome Back!
+                    Welcome!
                 </Typography>
                 <TextField
                     label="Username"
